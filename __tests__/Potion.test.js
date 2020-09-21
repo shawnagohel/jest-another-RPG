33517ebfbba2aa62e298e634528b3d1498584269
shawnagohel <shawna.gohel@gmail.com>
2020-09-21 15:03:07 -0700

    potion

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+const Potion = require('/Users/shawnagohel/Desktop/module_10/lib/Potion.js');
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
